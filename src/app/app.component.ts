3bcd81e0eb5e8f8a56fced316f92044bd57e222e
Abhilash A S <abhilashatl57@gmail.com>
2024-07-03 22:36:10 +0530

    Particles js implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { PARTICLES_JSON } from './particles/particles-json';
+
+declare var particlesJS: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'majestic-warhorse';
+  ngAfterViewInit() {
+    particlesJS('particles-js', PARTICLES_JSON);
+  }
 }
