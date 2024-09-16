877f8a7cff880bf428be486a8e425d24f793c6c6
ajithkumar <rsajithtvm@gmail.com>
2024-09-16 14:47:09 +0530

    app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,10 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { authGuard } from './auth.guard/guards/auth.guard';
 import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
 import { RegistrationPageComponent } from './pages/registration-page/registration-page.component';
-import { CoursesComponent } from './pages/courses/courses.component';
-
 
 const routes: Routes = [
-  { path: '', redirectTo: '/courses', pathMatch: 'full' },
-  { path: 'courses', component: CoursesComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginPageComponent },
   { path: 'forgetpassword', component: ForgotPasswordComponent },
   { path: 'signup', component: RegistrationPageComponent },
   {
