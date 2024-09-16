e6790a8e05755620630869deea4d8ebb35e157ea
ajithkumar <rsajithtvm@gmail.com>
2024-09-16 14:33:01 +0530

    courses list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,12 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { authGuard } from './auth.guard/guards/auth.guard';
 import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
 import { RegistrationPageComponent } from './pages/registration-page/registration-page.component';
+import { CoursesComponent } from './pages/courses/courses.component';
+
 
 const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: LoginPageComponent },
+  { path: '', redirectTo: '/courses', pathMatch: 'full' },
+  { path: 'courses', component: CoursesComponent },
   { path: 'forgetpassword', component: ForgotPasswordComponent },
   { path: 'signup', component: RegistrationPageComponent },
   {
