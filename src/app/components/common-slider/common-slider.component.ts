607634d91a747fb1ab2f9393574d4e1782c6a674
ajithkumar <rsajithtvm@gmail.com>
2024-10-31 14:45:35 +0530

    common slider

---
+++ src/app/components/common-slider/common-slider.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CourseUploadComponent } from './../../pages/course-upload/course-upload.component';
+@Component({
+  selector: 'app-common-slider',
+  standalone: true,
+  imports: [CourseUploadComponent],
+  templateUrl: './common-slider.component.html',
+  styleUrl: './common-slider.component.scss'
+})
+export class CommonSliderComponent {
+
+}
