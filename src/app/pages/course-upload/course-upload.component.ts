91df504c48f68d039a184cb65ad13c2e3fd77339
Abhilash A S <45873495+Abhilash-A-S@users.noreply.github.com>
2024-11-03 20:21:51 +0530

    Slider Implementation (#41)
    * Course-upload save implementation
    
    * Slider Implementation

--- src/app/pages/course-upload/course-upload.component.ts
+++ src/app/pages/course-upload/course-upload.component.ts
@@ -2,16 +2,25 @@ import { CommonSliderComponent } from './../../components/common-slider/common-s
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-
+import { AttachmentAccordionComponent } from 'src/app/components/attachment-accordion/attachment-accordion.component';
+import { IChapterInfo } from './model/chapter-info';
+import { CourseUploadService } from './course-upload.service';
+import { IMainCourseInfo } from './model/course-info';
 @Component({
   selector: 'app-course-upload',
   standalone: true,
-  imports: [FormsModule, CommonModule, CommonSliderComponent],
+  imports: [FormsModule, CommonModule, CommonSliderComponent,AttachmentAccordionComponent],
   templateUrl: './course-upload.component.html',
   styleUrl: './course-upload.component.scss',
 })
 export class CourseUploadComponent {
   public mobMenu: boolean = false;
+  public mainCourseInfo: IMainCourseInfo;
+  public courseChapterList: IChapterInfo[] = [];
+  constructor(private courseUploadService: CourseUploadService) {
+    this.mainCourseInfo = this.courseUploadService.MAIN_COURSE_INFO;
+    this.addNewChapter();
+  }
   @ViewChild('btnTrigger', { static: true }) btnTrigger!: ElementRef<HTMLButtonElement>;
   triggerMenu() {
     this.btnTrigger.nativeElement.click();
@@ -20,5 +29,10 @@ export class CourseUploadComponent {
   mobileMenu() {
     this.mobMenu = !this.mobMenu;
   }
-
+  addNewVideoList(chapter: IChapterInfo) {
+    chapter.fileDetails = chapter.fileDetails.concat(this.courseUploadService.FILE_OBJECT_INFO);
+  }
+  addNewChapter() {
+    this.courseChapterList = this.courseChapterList.concat(this.courseUploadService.CHAPTER_INFO);
+  }
 }
