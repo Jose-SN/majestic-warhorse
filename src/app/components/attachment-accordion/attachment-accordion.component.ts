123fa5ef94d8d03941b313c9f5a7de323541ca28
Abhilash A S <45873495+Abhilash-A-S@users.noreply.github.com>
2024-11-14 22:59:34 +0530

    build fixes (#50)

--- src/app/components/attachment-accordion/attachment-accordion.component.ts
+++ src/app/components/attachment-accordion/attachment-accordion.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IAttachmentObjectInfo } from 'src/app/pages/course-upload/model/file-object-info';
 
 @Component({
   selector: 'app-attachment-accordion',
@@ -8,6 +9,7 @@ import { Component } from '@angular/core';
   styleUrl: './attachment-accordion.component.scss',
 })
 export class AttachmentAccordionComponent {
+  @Input() attachmentList: IAttachmentObjectInfo[] = [];
   toggleAccordian(event: any, index: any) {
     const element = event.target;
     element.classList.toggle('active');
