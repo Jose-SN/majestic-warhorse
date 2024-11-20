6f9183dd92638af5870b5d77ce90a920da6aa894
Abhilash A S <45873495+Abhilash-A-S@users.noreply.github.com>
2024-11-20 22:56:15 +0530

    Status saver (#51)
    * build fixes
    
    * status save

--- src/app/pages/course-details/course-details.service.ts
+++ src/app/pages/course-details/course-details.service.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { CoursesApiService } from 'src/app/services/api-service/courses-api.service';
+import { CommonService } from 'src/app/shared/services/common.service';
+import { TOASTER_MESSAGE_TYPE } from 'src/app/shared/toaster/toaster-info';
+import { ICourseStatus } from './model/course-status';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CourseDetailsService {
-  constructor() {}
+  public courseStatusList: ICourseStatus[] = [];
+  public reconfigureStatus$: Subject<any> = new Subject<true>();
+  constructor(
+    private commonService: CommonService,
+    private courseApiService: CoursesApiService
+  ) {}
   formatDuration(seconds: number): string {
     const hours = Math.floor(seconds / 3600);
     const minutes = Math.floor((seconds % 3600) / 60);
@@ -14,5 +24,61 @@ export class CourseDetailsService {
     const secondsPart =
       remainingSeconds > 0 ? `${remainingSeconds} second${remainingSeconds !== 1 ? 's' : ''}` : '';
     return [hoursPart, minutesPart, secondsPart].filter(Boolean).join(' ').trim();
+  }
+  saveCourseRating(saveInfo: any, destroy$: Subject<void>) {
+    const createdId = this.commonService.loginedUserInfo.id;
+    let service;
+    let statusPayload;
+    if (saveInfo.isVideo) {
+      statusPayload = {
+        rating: null,
+        createdBy: createdId,
+        parentId: saveInfo.activeFile._id,
+        parentType: 'Chapter',
+        percentage:
+          saveInfo.courseStatusInfo.percentage !== 100 ? saveInfo.videoPercentage || 0 : 100,
+        ...(saveInfo.videoStatusInfo._id && { _id: saveInfo.videoStatusInfo._id }),
+      };
+    } else {
+      statusPayload = {
+        percentage: null,
+        createdBy: createdId,
+        parentType: 'Chapter',
+        rating: saveInfo.rating,
+        parentId: saveInfo.activeChapter._id,
+        ...(saveInfo.courseStatusInfo._id && { _id: saveInfo.courseStatusInfo._id }),
+      };
+    }
+    if (statusPayload._id) {
+      service = this.courseApiService.updateCourseStatus.bind(this.courseApiService);
+    } else {
+      service = this.courseApiService.saveCourseStatus.bind(this.courseApiService);
+    }
+    service(statusPayload)
+      .pipe(takeUntil(destroy$))
+      .subscribe({
+        next: (success) => {
+          if (!saveInfo.isVideo) {
+            this.commonService.openToaster({
+              message: 'Succesfully updated the status of the Course',
+              messageType: TOASTER_MESSAGE_TYPE.SUCCESS,
+            });
+          }
+          this.getCourseStatusList();
+        },
+        error: (error) => {
+          this.commonService.openToaster({
+            message: 'Error while updating the status of the course',
+            messageType: TOASTER_MESSAGE_TYPE.ERROR,
+          });
+        },
+      });
+  }
+  async getCourseStatusList() {
+    this.courseStatusList = await this.courseApiService.getCourseStatusList();
+    this.reconfigureStatus$.next(true);
+  }
+  getReconfigurationHandler() {
+    return this.reconfigureStatus$.asObservable();
   }
 }
--- src/app/services/api-service/courses-api.service.ts
+++ src/app/services/api-service/courses-api.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, lastValueFrom } from 'rxjs';
+import { ICourseStatus } from 'src/app/pages/course-details/model/course-status';
 import { ICourseList, IcourseListResponse } from 'src/app/pages/courses/modal/course-list';
 import { CommonService } from 'src/app/shared/services/common.service';
 import { environment } from 'src/environments/environment';
@@ -33,6 +34,23 @@ export class CoursesApiService {
         .get<
           ICourseList[]
         >(`${this._apiUrl}course/get?populateUser=true&populateChapters=true&populateFiles=true`)
+        .pipe(catchError(this.commonService.handleError))
+    );
+  }
+  saveCourseStatus(courseUploadPayload: any) {
+    return this.http
+      .post<ICourseStatus>(`${this._apiUrl}status/save`, courseUploadPayload)
+      .pipe(catchError(this.commonService.handleError));
+  }
+  updateCourseStatus(courseUploadPayload: any) {
+    return this.http
+      .post<ICourseStatus>(`${this._apiUrl}status/update`, courseUploadPayload)
+      .pipe(catchError(this.commonService.handleError));
+  }
+  getCourseStatusList() {
+    return lastValueFrom(
+      this.http
+        .get<ICourseStatus[]>(`${this._apiUrl}status/get`)
         .pipe(catchError(this.commonService.handleError))
     );
   }
