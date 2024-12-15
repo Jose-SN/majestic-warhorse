9e64b5c310866367fbf1161a2a4b725e211240af
Jose SN <dzine.jose@gmail.com>
2024-12-15 19:36:46 +0000

    Update dashboard.component.ts

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -86,7 +86,7 @@ export class DashboardComponent implements OnInit, OnDestroy {
       this.activePanel = this.SIDE_PANEL_LIST['APPROVAL_PENDING'];
       this.infoMessage =
         'You have not been assigned any teachers to view this course. Please contact the admin for assistance';
-    } else if ((loginedUserData?.role === 'teacher' && !loginedUserData.approved) || true) {
+    } else if ((loginedUserData?.role === 'teacher' && !loginedUserData.approved)) {
       this.activePanel = this.SIDE_PANEL_LIST['APPROVAL_PENDING'];
       this.infoMessage =
         'Your request is pending approval from the admin. Please reach out to the admin for assistance.';
